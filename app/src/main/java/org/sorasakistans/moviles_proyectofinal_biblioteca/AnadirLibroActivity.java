736c984e2379532f615cb9bf74c6cfac7d86685b
KYRR18 <kevin.rosasro@alumno.buap.mx>
2026-05-20 13:46:34 -0600

    Logica para conectarse con el servidor php

--- app/src/main/java/org/sorasakistans/moviles_proyectofinal_biblioteca/AnadirLibroActivity.java
+++ app/src/main/java/org/sorasakistans/moviles_proyectofinal_biblioteca/AnadirLibroActivity.java
@@ -1,33 +1,85 @@
 package org.sorasakistans.moviles_proyectofinal_biblioteca;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class AnadirLibroActivity extends AppCompatActivity {
-
+    ImageView back;
+    ImageView scan;
+    TextInputEditText title,autor,editor,isbn;
+    MaterialButton cancel,ok;
+    ImageView ivList, ivHome, ivSearch, ivUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.anadir_libro);
-    }
 
-    public void crearLibro(String isbn, String titulo, String autor, String editorial,View v) {
+        back = findViewById(R.id.btnBack);
+        scan = findViewById(R.id.btnScan);
+        title = findViewById(R.id.etTitle);
+        autor = findViewById(R.id.etAuthor);
+        editor = findViewById(R.id.etPublisher);
+        isbn = findViewById(R.id.etIsbn);
+        cancel = findViewById(R.id.btnCancel);
+        ok = findViewById(R.id.btnConfirm);
+        ivList = findViewById(R.id.iv_list);
+        ivHome = findViewById(R.id.iv_home);
+        ivSearch = findViewById(R.id.iv_search);
+        ivUser = findViewById(R.id.iv_user);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {finish();}
+        });
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {finish();}
+        });
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                verificarData(view);
+            }
+        });
+    }
+    public void verificarData(View view){
+        String isb = isbn.getText().toString();
+        String titulo =title.getText().toString();
+        String aut = autor.getText().toString();
+        String editorial = editor.getText().toString();
+        String error = "";
+        if (isb.isEmpty()){error+="ISBN vaci\n";}
+        if (titulo.isEmpty()){error+="Titulo vacio\n";}
+        if (aut.isEmpty()){error+="Autor vacio\n";}
+        if (editorial.isEmpty()){error+="Editorial vacio\n";}
+        if (error.equals("")){crearLibro(isb,titulo,aut,editorial);}
+        else{alerta(error, "Error");}
+    }
+    public void crearLibro(String isbn, String titulo, String autor, String editorial) {
         String url = "http://10.0.2.2/api_biblioteca/api/crear_libro.php";//VM RECUERDA VM ONLY
         // 1. Crear el objeto JSON que enviaremos
         JSONObject jsonBody = new JSONObject();
@@ -64,5 +116,17 @@ public class AnadirLibroActivity extends AppCompatActivity {
         // 3. Añadir la petición a la cola de Volley
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         requestQueue.add(jsonObjectRequest);
+    }
+    public void alerta(String line , String titulo){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(titulo)
+                .setMessage(line)
+                .setCancelable(true)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //
+                    }
+                }).show();
     }
 }
--- app/src/main/java/org/sorasakistans/moviles_proyectofinal_biblioteca/ModificarLibroActivity.java
+++ app/src/main/java/org/sorasakistans/moviles_proyectofinal_biblioteca/ModificarLibroActivity.java
@@ -1,15 +1,128 @@
 package org.sorasakistans.moviles_proyectofinal_biblioteca;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class ModificarLibroActivity extends AppCompatActivity {
+    ImageView back, trash, options;
+    TextInputEditText title,autor,editor,isbn;
+    MaterialButton cancel,ok;
+    ImageView ivList, ivHome, ivSearch, ivUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.modificar_libro);
-
         super.onCreate(savedInstanceState);
+        back = findViewById(R.id.btnBack);
+        trash = findViewById(R.id.btnDeleteBook);
+        options = findViewById(R.id.btnMoreOptions);
+        title = findViewById(R.id.etTitleMod);
+        autor = findViewById(R.id.etAuthorMod);
+        editor = findViewById(R.id.etPublisherMod);
+        isbn = findViewById(R.id.etIsbnMod);
+        cancel = findViewById(R.id.btnCancelMod);
+        ok = findViewById(R.id.btnConfirmMod);
+        ivList = findViewById(R.id.iv_listMod);
+        ivHome = findViewById(R.id.iv_homeMod);
+        ivSearch = findViewById(R.id.iv_searchMod);
+        ivUser = findViewById(R.id.iv_userMod);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {finish();}
+        });
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {finish();}
+        });
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                verificarData(view);
+            }
+        });
     }
+    public void verificarData(View view){
+        String isb = isbn.getText().toString();
+        String titulo =title.getText().toString();
+        String aut = autor.getText().toString();
+        String editorial = editor.getText().toString();
+        String error = "";
+        if (isb.isEmpty()){error+="ISBN vaci\n";}
+        if (titulo.isEmpty()){error+="Titulo vacio\n";}
+        if (aut.isEmpty()){error+="Autor vacio\n";}
+        if (editorial.isEmpty()){error+="Editorial vacio\n";}
+        if (error.equals("")){modificarLibro(isb,titulo,aut,editorial);}
+        else{alerta(error, "Error");}
+    }
+    public void modificarLibro(String isbn, String titulo, String autor, String editorial) {
+        String url = "http://10.0.2.2/api_biblioteca/api/modificar_libro.php";//VM RECUERDA VM ONLY
+        // 1. Crear el objeto JSON que enviaremos
+        JSONObject jsonBody = new JSONObject();
+        try {
+            jsonBody.put("isbn", isbn);
+            jsonBody.put("titulo", titulo);
+            jsonBody.put("autor", autor);
+            jsonBody.put("editorial", editorial);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        // 2. Crear la petición
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, jsonBody,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            boolean exito = response.getBoolean("exito");
+                            String mensaje = response.getString("mensaje");
+                            // Mostrar mensaje al usuario (ej. Toast)
+                            //Toast tosty = new Toast(v.getContext());
+                            //tosty.
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // Manejar error de red o servidor (400, 500, etc.)
+                error.printStackTrace();
+            }
+        });
+        // 3. Añadir la petición a la cola de Volley
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(jsonObjectRequest);
+    }
+    public void alerta(String line , String titulo){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(titulo)
+                .setMessage(line)
+                .setCancelable(true)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //
+                    }
+                }).show();
+    }
+
+
+
 }
